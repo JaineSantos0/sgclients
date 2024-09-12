6196a6732a8b9c77874831064a573956f8b42c75
Jaine Santos <jainejosiane@gmail.com>
2024-09-12 16:26:24 -0300

    first commit

---
+++ src/main/java/com/jainejosiane/sgclients/SgclientsApplication.java
@@ -0,0 +1,13 @@
+package com.jainejosiane.sgclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgclientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgclientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jainejosiane/sgclients/SgclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jainejosiane.sgclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
